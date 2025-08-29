bcf20eedc6e30386014896fec99fac51a9f2d5b7
tusheralltime <tushermolla200@gmail.com>
2025-08-29 15:42:42 +0600

    added clickable copy button increase countnumber

--- js/index.js
+++ js/index.js
@@ -6,11 +6,25 @@ let heartCount = 0;
 let coinCount = 100;
 let copyCount = 0;
 
-//  Heart Button click then increase the count 
+//  Heart Button click then increase the count
 const heartButtons = document.getElementsByClassName("heart-button");
 for (let button of heartButtons) {
   button.addEventListener("click", function () {
     heartCount++;
     getElement("heart-count").innerText = heartCount;
+  });
+}
+
+// Copy Button
+const copyButtons = document.getElementsByClassName("copy-button");
+for (let button of copyButtons) {
+  button.addEventListener("click", function () {
+    const card = button.parentNode.parentNode;
+    const helplineNumber = card.querySelector(".helpline-number").innerText;
+
+    navigator.clipboard.writeText(helplineNumber);
+    copyCount++;
+    getElement("copy-count").innerText = copyCount;
+    alert(`Number ${helplineNumber} copied!`);
   });
 }
